6a900aa8d4e4ba09920e732e009445034ac379aa
Francesco <sennozeta@gmail.com>
2021-02-23 16:17:22 +0000

    ProgressRings working

--- src/components/Stats/Stat/Stat.js
+++ src/components/Stats/Stat/Stat.js
@@ -1,26 +1,49 @@
-import React, {Fragment} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import classes from './css/Stat.module.css'
 
-const Stat = ({stat, percentage}) => {
-    return (
-    <>
-        <div className={classes.BigCircle}>
-            <svg height="95" width="95">
-                <circle cx="47.5" cy="47" r="38"/>
-                <circle cx="47.5" cy="47" r="38"/>
-            </svg> 
-            <div className={classes.MiddleCircle}>
-                <div className={classes.SmallCircle}>
-                        <p data-number={percentage}>%</p>
-                    </div>
 
-                    <div className={classes.TextBox}>
-                        <p>{stat}</p>
-                    </div>
+const Stat = (props) => {
+
+const radius = 52;
+const circumference = radius * 2 * Math.PI;
+
+let [offset, setOffset] = useState(0);
+
+useEffect(()=>{
+    setProgress(props.percentage);
+})
+
+const setProgress = (percent) => {
+     setOffset(circumference - percent / 100 * circumference);
+}
+
+let circle = (
+        <circle
+            style={{
+                strokeDasharray: `${circumference} ${circumference}`,
+                strokeDashoffset: `${offset}`,
+            }}
+            className={classes.ProgressRingCircle}
+            r={radius}
+            cx="60"
+            cy="60"
+        />
+    );
+
+    return (
+        <div className={classes.Container}>
+            <div className={classes.OutsideCircle}>
+                <svg
+                width="120"
+                height="120">
+                    {circle}
+                </svg>
+                <p>{props.percentage}%</p>
             </div>
+
+            <div className={classes.StatTitle}>{props.stat}</div>
         </div>
-    </>
 
     )
 }
--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Stat from './Stat/Stat'
 
 import classes from './css/Stats.module.css'
 
-const Stats = () => {
+const Stats = (props) => {
+
+    const [percentage, setPercentage] = useState(0);
+
+    let ranNum = () => {
+        return Math.round(Math.random() * 100);
+    }
+    
+    useEffect(()=>{
+        setPercentage(ranNum());
+    }, [])
+    
     return (
         <div className={classes.Stats}>
-            <Stat stat="Happy" percentage="38"/>
-            <Stat stat="Smart" percentage="68"/>
-            <Stat stat="Look" percentage="20"/>
-            <Stat stat="Health" percentage="86"/>
+            <Stat stat="Happy" percentage={percentage}/>
+            <Stat stat="Smart" percentage={25}/>
+            <Stat stat="Look" percentage={40}/>
+            <Stat stat="Health" percentage={80}/>
         </div>
     )
 }
